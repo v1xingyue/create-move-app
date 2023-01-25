955e582b792a43e95fd1302e1d05b06155f82de7
v1xingyue <qixingyue@126.com>
2023-01-25 16:38:45 +0800

    init

---
+++ appMapping.js
@@ -0,0 +1,18 @@
+export default {
+    "aptos": {
+        "url": "https://github.com/NonceGeek/scaffold-move.git",
+        "cwd": "scaffold-aptos",
+        "cmds": [
+            "npm install"
+        ],
+        "start": "npm run dev"
+    },
+    "sui": {
+        "url": "https://github.com/NonceGeek/scaffold-move.git",
+        "cwd": "scaffold-sui",
+        "cmds": [
+            "npm install"
+        ],
+        "start": "npm run dev"
+    },
+}
---
+++ index.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+
+import chalk from 'chalk';
+import path from 'path';
+import { execSync } from "child_process";
+import yargs from 'yargs/yargs';
+import { hideBin } from 'yargs/helpers';
+import { appMapping } from "./appMapping";
+
+const runCommand = (command, { cwd }) => {
+    cwd = cwd || ".";
+    try {
+        execSync(command, { stdio: 'inherit', cwd });
+    } catch (error) {
+        console.log(`Failed to execute command  ${command} `, error);
+        return false;
+    }
+    return true;
+}
+
+const createApp = ({ name, chain }) => {
+    console.log("create move app <%s> on [%s]", chalk.blue(name), chalk.green(chain));
+    if (appMapping[chain]) {
+        let { url, cwd, cmds, start } = appMapping[chain];
+        console.log(chalk.green("clone from ", url));
+        runCommand(`git clone ${url} ${name} `, {});
+        cwd = path.join(name, cwd);
+        for (let cmd of cmds) {
+            console.log(cwd);
+            runCommand(cmd, { cwd });
+        }
+        console.log(
+            chalk.green("\n You app has been sucessfully created. Run following command to start app. \n")
+        );
+        console.log("\n")
+        console.log(`cd ${cwd}`);
+        console.log(`${start} `);
+
+    } else {
+        console.error(chalk.red("Sorry, no support for chain ", chain));
+    }
+}
+
+yargs(hideBin(process.argv))
+    .command('* [name]', 'create move app', (yargs) => {
+        return yargs
+            .positional('name', {
+                describe: 'name for move app',
+                default: "hellomove"
+            })
+    }, (argv) => {
+        createApp({ ...argv })
+    })
+    .option('chain', {
+        alias: 'c',
+        type: 'string',
+        description: 'Blockchain you want to run.',
+        default: "aptos",
+    })
+    .parse();
+

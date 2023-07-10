fda69474f3a45c134f4fbac694bd3efb0eda56a7
v1xingyue <qixingyue@126.com>
2023-07-10 12:03:56 +0800

    update version

--- appMapping.js
+++ appMapping.js
@@ -1,34 +1,26 @@
 export default {
-    "aptos": {
-        "url": "https://github.com/NonceGeek/scaffold-move.git",
-        "cwd": "",
-        "cmds": [
-            "yarn install || npm install"
-        ],
-        "start": "npm run dev"
-    },
-    "sui": {
-        "url": "https://github.com/NonceGeek/scaffold-sui.git",
-        "cwd": "",
-        "cmds": [
-            "yarn install || npm install"
-        ],
-        "start": "npm run dev"
-    },
-    "starcoin": {
-        "url": "https://github.com/v1xingyue/movegroup.git",
-        "cwd": "dapp",
-        "cmds": [
-            "yarn install || npm install"
-        ],
-        "start": "npm run dev"
-    },
-    "page": {
-        "url": "https://github.com/v1xingyue/move-page.git",
-        "cwd": "",
-        "cmds": [
-            "yarn install || npm install"
-        ],
-        "start": "npm run dev"
-    }
-}
+  aptos: {
+    url: "https://github.com/movefuns/aptos-dapp-scaffold.git",
+    cwd: "",
+    cmds: ["yarn install || npm install"],
+    start: "npm run dev",
+  },
+  sui: {
+    url: "https://github.com/NonceGeek/scaffold-sui.git",
+    cwd: "",
+    cmds: ["yarn install || npm install"],
+    start: "npm run dev",
+  },
+  starcoin: {
+    url: "https://github.com/v1xingyue/movegroup.git",
+    cwd: "dapp",
+    cmds: ["yarn install || npm install"],
+    start: "npm run dev",
+  },
+  page: {
+    url: "https://github.com/v1xingyue/move-page.git",
+    cwd: "",
+    cmds: ["yarn install || npm install"],
+    start: "npm run dev",
+  },
+};
